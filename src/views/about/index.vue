d978c4dc49d321ba20fb971f92b9c58bc98a7a49
yx1126 <yx17714503091@163.com>
2025-03-06 17:39:33 +0800

    feat: add about page

---
+++ src/views/about/index.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import packageJson from "../../../package.json";
+
+defineOptions({
+    name: "About",
+});
+
+const { t } = useI18n();
+</script>
+
+<template>
+    <div class="about flex flex-col gap-[10px]">
+        <el-card :header="t('prodTitle')" shadow="never">
+            <el-descriptions border label-width="250">
+                <template v-for="(v, k) in packageJson.dependencies" :key="k">
+                    <el-descriptions-item :label="k">
+                        <span>{{ v }}</span>
+                    </el-descriptions-item>
+                </template>
+            </el-descriptions>
+        </el-card>
+        <el-card :header="t('devTitle')" shadow="never">
+            <el-descriptions border label-width="250">
+                <template v-for="(v, k) in packageJson.devDependencies" :key="k">
+                    <el-descriptions-item :label="k">
+                        <span>{{ v }}</span>
+                    </el-descriptions-item>
+                </template>
+            </el-descriptions>
+        </el-card>
+    </div>
+</template>
+
+<i18n lang="yaml">
+zh:
+  devTitle: 开发环境依赖
+  prodTitle: 生产环境依赖
+enUS:
+  devTitle: dependencies
+  prodTitle: 生产环境依赖
+</i18n>
